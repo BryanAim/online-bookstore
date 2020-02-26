a6495944ff42764bf19b4a9ac76e2c072ca494e1
Isale <isale.bryan@gmail.com>
2020-02-26 22:38:07 +0300

    color change, homepage image swap

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,5 @@
 <div id="footer-wrap">
 <p id= "legal">
-Ololchike Online Bookstore 2020</p>
-	<!-- <p id="legal">Online Book Store (c)2016 Design by <a href="developer.php">Biswas Himel, Ahmed Md Sabbir</a>.</p> -->
+Ololchike Bookstore 2020</p>
+	<!-- <p id="legal">Book Store (c)2016 Design by <a href="developer.php">Biswas Himel, Ahmed Md Sabbir</a>.</p> -->
 	</div>
